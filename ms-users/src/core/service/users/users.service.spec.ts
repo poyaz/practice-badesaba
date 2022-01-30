0f34d47ce71780be28dfe2664b20aa2a21cff09e
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-01-30 17:42:44 +0330

    Add TDD unit test for UsersServiceService.getById

--- ms-users/src/core/service/users/users.service.spec.ts
+++ ms-users/src/core/service/users/users.service.spec.ts
@@ -1,12 +1,31 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users-service.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {mock, MockProxy} from 'jest-mock-extended';
+import {UsersService} from './users.service';
+import {IUserRepositoryInterface, USERS_REPOSITORY} from '../../interface/i-user-repository.interface';
+import {IIdentifierGenerator} from '../../interface/i-identifier-generator.interface';
+import {UnknownException} from '../../exception/unknown-exception';
+import {NotFoundException} from '../../exception/not-found-exception';
+import {UsersModel} from '../../model/usersModel';
 
 describe('UsersServiceService', () => {
   let service: UsersService;
+  let userRepository: MockProxy<IUserRepositoryInterface>;
+  let identifierGeneratorMock: MockProxy<IIdentifierGenerator>;
 
   beforeEach(async () => {
+    userRepository = mock<IUserRepositoryInterface>();
+
+    identifierGeneratorMock = mock<IIdentifierGenerator>();
+    identifierGeneratorMock.generateId.mockReturnValue('00000000-0000-0000-0000-000000000000');
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: USERS_REPOSITORY,
+          useValue: userRepository,
+        }
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -14,5 +33,39 @@ describe('UsersServiceService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('get user by id', () => {
+    it('should error get user by id', async () => {
+      const id = identifierGeneratorMock.generateId();
+      userRepository.getById.mockResolvedValue([new UnknownException()]);
+
+      const [error] = await service.getById(id);
+
+      expect(userRepository.getById).toHaveBeenCalled();
+      expect(error).toBeInstanceOf(UnknownException);
+    });
+
+    it('should error not found when user not exist by id', async () => {
+      const id = identifierGeneratorMock.generateId();
+      userRepository.getById.mockResolvedValue([null]);
+
+      const [error] = await service.getById(id);
+
+      expect(userRepository.getById).toHaveBeenCalled();
+      expect(error).toBeInstanceOf(NotFoundException);
+    });
+
+    it('should successful get user by id', async () => {
+      const id = identifierGeneratorMock.generateId();
+      const outputModel = new UsersModel();
+      userRepository.getById.mockResolvedValue([null, outputModel]);
+
+      const [error, result] = await service.getById(id);
+
+      expect(userRepository.getById).toHaveBeenCalled();
+      expect(error).toEqual(null);
+      expect(result).toBeInstanceOf(UsersModel);
+    });
   });
 });
--- ms-users/src/core/service/users/users.service.ts
+++ ms-users/src/core/service/users/users.service.ts
@@ -1,9 +1,17 @@
-import {Injectable} from '@nestjs/common';
-import {IUsersService} from "../../interface/i-users-service.interface";
-import {UsersModel} from "../../model/usersModel";
+import {Inject, Injectable} from '@nestjs/common';
+import {IUsersService} from '../../interface/i-users-service.interface';
+import {UsersModel} from '../../model/usersModel';
+import {IUserRepositoryInterface, USERS_REPOSITORY} from '../../interface/i-user-repository.interface';
+import {NotFoundException} from '../../exception/not-found-exception';
 
 @Injectable()
 export class UsersService implements IUsersService {
+  constructor(
+    @Inject(USERS_REPOSITORY)
+    private readonly _usersRepository: IUserRepositoryInterface
+  ) {
+  }
+
   add(model: UsersModel): Promise<(Error | UsersModel)[]> {
     return Promise.resolve([]);
   }
@@ -16,8 +24,16 @@ export class UsersService implements IUsersService {
     return Promise.resolve([]);
   }
 
-  getById(id: string): Promise<(Error | UsersModel)[]> {
-    return Promise.resolve([]);
+  async getById(id: string): Promise<(Error | UsersModel)[]> {
+    const [error, data] = await this._usersRepository.getById(id);
+    if (error) {
+      return [error];
+    }
+    if (!data) {
+      return [new NotFoundException()];
+    }
+
+    return [null, data];
   }
 
   update(model: UsersModel): Promise<Error[]> {
