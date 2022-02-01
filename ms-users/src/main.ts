2d39511566dc768f16a8b60bd84bd7f2b2e2255a
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-02-01 10:55:33 +0330

    Add support rpc
    Add env and config for rpc

--- ms-users/src/main.ts
+++ ms-users/src/main.ts
@@ -2,14 +2,26 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {ValidationPipe} from '@nestjs/common';
 import {Config} from './module/config';
+import {Transport} from '@nestjs/microservices';
 
 const IP_ADDRESS = Config.getStr('server.host', '127.0.0.1');
-const PORT_BIND = Config.getNum('server.http.port', 3000);
+const HTTP_PORT_BIND = Config.getNum('server.http.port', 3000);
+const RPC_PORT_BIND = Config.getNum('server.rpc.port', 8888);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(PORT_BIND, IP_ADDRESS);
+  const http = await NestFactory.create(AppModule);
+  http.useGlobalPipes(new ValidationPipe());
+  await http.listen(HTTP_PORT_BIND, IP_ADDRESS);
+
+  const message = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: IP_ADDRESS,
+      port: RPC_PORT_BIND,
+    },
+  });
+  message.useGlobalPipes(new ValidationPipe());
+  await message.listen();
 }
 
 bootstrap().catch((error) => {
